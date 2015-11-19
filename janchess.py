bb6a792ee8536e6e38bfba311d93d93925a12eb6
xjcl <janxjcl@gmail.com>
2015-11-19 05:32:47 +0100

    make random legal move (no ep/pp)

--- janchess.py
+++ janchess.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 
+
+# XXX automate is_inside checks on board
+
 import time
 import logging
 import random
 import sys
-import contextlib
+# import contextlib
+import itertools
 
 logging.basicConfig(filename='janchess.log', level=logging.DEBUG)
 logging.info(str(time.time()))
@@ -35,6 +39,28 @@ LETTERMAP = {0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h'}
 LETTERMAPINV = {v:k for k,v in LETTERMAP.items()}
 
 
+
+WHITEKINGMOVED = False
+BLACKKINGMOVED = False
+
+# XXX used for en-passant
+LASTMOVE = None
+
+board = None
+IM_WHITE = False
+started = True
+BUF = ''
+
+
+
+def is_inside(i, j):
+    return 0 <= i <= 7 and 0 <= j <= 7
+
+# DON'T NEGATE
+def is_empty(i, j):
+    return is_inside(i, j) and not board[i][j]
+
+
 def new_board():
     return [
         [BLACK + ROOK, BLACK + KNIGHT, BLACK + BISHOP, BLACK + QUEEN,
@@ -50,49 +76,209 @@ def new_board():
     ]
 
 
+def gentuples():
+    return itertools.product(range(7), range(7))
+
 def input_is_move(i):
     return len(i) >= 4 and i[0] in 'abcdefgh' and i[1] in '12345678' \
         and i[2] in 'abcdefgh' and i[3] in '12345678'
 
+def sq_to_str1(sq):
+    if sq & BLACK: return 'B'
+    if sq & WHITE: return 'W'
+    return ' '
+
+def sq_to_str2(sq):
+    if sq & PAWN: return 'P'
+    if sq & KNIGHT: return 'N'
+    if sq & BISHOP: return 'B'
+    if sq & ROOK: return 'R'
+    if sq & QUEEN: return 'Q'
+    if sq & KING: return 'K'
+    return ' '
+
 def pprint():
-    [print(row) for row in board]
+    print()
+    for row in board:
+        print(' '.join(sq_to_str1(sq) + sq_to_str2(sq) for sq in row))
+    print()
 
 
 
-def gen_legals(white_turn=False):
-    COLOR = WHITE if white_turn else BLACK
-    ADD = -1 if white_turn else 1
 
-    ret = list()
+
+def king_not_in_check(col=...):
+
+    col  = WHITE if IM_WHITE else BLACK
+    ncol = WHITE if not IM_WHITE else BLACK
+    myking = KING + col
+    ADD = -1 if IM_WHITE else 1
+
     for i, row in enumerate(board):
         for j, col in enumerate(row):
-            if 1 <= i <= 6 and 0 <= j <= 7:
-                if board[i][j] & PAWN and board[i][j] & COLOR:
-                    if not board[i+ADD][j]:
+            bij = board[i][j]
+            if bij & ncol:
+                if bij & PAWN:
+                    if is_inside(i-ADD, j+1) and board[i-ADD][j+1] == myking:
+                        return False
+                    if is_inside(i-ADD, j-1) and board[i-ADD][j-1] == myking:
+                        return False
+                elif bij & KNIGHT:
+                    for x,y in ((1,2), (2,1), (-1,2), (2,-1), (-1,-2), (-2,-1), (1,-2), (-2,1)):
+                        if is_inside(i+x, j+y):
+                            if board[i+x][j+y] == myking:
+                                return False
+                elif bij & BISHOP or bij & QUEEN:
+                    for a,b in ((1,1), (1,-1), (-1,1), (-1,-1)):
+                        for k in range(1, 8):
+                            if is_inside(i+a*k, j+b*k):
+                                if board[i+a*k][j+b*k] == myking:
+                                    return False
+                                elif board[i+a*k][j+b*k]:
+                                    break  # important
+                            else:
+                                break
+                if bij & ROOK or bij & QUEEN:
+                    for a,b in ((0,1), (1,0), (0,-1), (-1,0)):
+                        for k in range(1, 8):
+                            if is_inside(i+a*k, j+b*k):
+                                if board[i+a*k][j+b*k] == myking:
+                                    return False
+                                elif board[i+a*k][j+b*k]:
+                                    break  # important
+                            else:
+                                break
+                elif bij & KING:
+                    for a,b in ((0,1), (1,0), (0,-1), (-1,0), (1,1), (-1,-1), (1,-1), (-1,1)):
+                        if is_inside(i+a, j+b):
+                            if board[i+a][j+b] == myking:
+                                return False
+
+    return True
+
+
+# XXX add col
+def gen_legals():
+    COLOR  = WHITE if IM_WHITE else BLACK
+    NCOLOR = BLACK if IM_WHITE else WHITE
+    ADD = -1 if IM_WHITE else 1
+
+    ret = []
+
+    for i, row in enumerate(board):
+        for j, col in enumerate(row):
+            bij = board[i][j]
+            if bij & COLOR:
+                if bij & PAWN:
+                    if is_empty(i+ADD, j):
                         ret.append((i,j,i+ADD,j))
+                    if is_inside(i+ADD, j+1) and board[i+ADD][j+1] & NCOLOR:
+                        ret.append((i,j,i+ADD,j+1))
+                    if is_inside(i+ADD, j-1) and board[i+ADD][j-1] & NCOLOR:
+                        ret.append((i,j,i+ADD,j-1))
+                elif bij & KNIGHT:
+                    for x,y in ((1,2), (2,1), (-1,2), (2,-1), (-1,-2), (-2,-1), (1,-2), (-2,1)):
+                        if is_inside(i+x, j+y):
+                            if not board[i+x][j+y] or board[i+x][j+y] & NCOLOR:
+                                ret.append((i,j,i+x,j+y))
+                elif bij & BISHOP or bij & QUEEN:
+                    for a,b in ((1,1), (1,-1), (-1,1), (-1,-1)):
+                        for k in range(1, 8):
+                            if is_inside(i+a*k, j+b*k):
+                                if not board[i+a*k][j+b*k]:
+                                    ret.append((i,j,i+a*k,j+b*k))
+                                elif board[i+a*k][j+b*k] & NCOLOR:
+                                    ret.append((i,j,i+a*k,j+b*k))
+                                    break  # important
+                                elif board[i+a*k][j+b*k] & COLOR:
+                                    break  # important
+                            else:
+                                break
+                if bij & ROOK or bij & QUEEN:
+                    for a,b in ((0,1), (1,0), (0,-1), (-1,0)):
+                        for k in range(1, 8):
+                            if is_inside(i+a*k, j+b*k):
+                                if not board[i+a*k][j+b*k]:
+                                    ret.append((i,j,i+a*k,j+b*k))
+                                elif board[i+a*k][j+b*k] & NCOLOR:
+                                    ret.append((i,j,i+a*k,j+b*k))
+                                    break  # important
+                                elif board[i+a*k][j+b*k] & COLOR:
+                                    break  # important
+                            else:
+                                break
+                elif bij & KING:
+                    for a,b in ((0,1), (1,0), (0,-1), (-1,0), (1,1), (-1,-1), (1,-1), (-1,1)):
+                        if is_inside(i+a, j+b):
+                            if not board[i+a][j+b] or board[i+a][j+b] & NCOLOR:
+                                ret.append((i,j,i+a,j+b))
+
+
 
     # pprint()
     # print(ret)
 
-    if not ret:
-        print('Error (no legal moves): undo')
 
-    return ret
+    # XXX not moving into check
+
+    ret2 = []
+
+    for mv in ret:
+        hit_piece = make_move(mv)
+        assert not hit_piece & KING
+
+        if king_not_in_check():
+            ret2.append(mv)
+
+        unmake_move(mv, hit_piece)
+
+    print(ret, ret2)
+
+    if not ret2:
+        print('result 1/2-1/2')  # stalemate
+        logging.shutdown()
+        exit()
 
 
-def make_move(mv, white_turn=True):
-    piece = board [8 - int(mv[1])] [ LETTERMAPINV[mv[0]] ]
-    board [8 - int(mv[1])] [ LETTERMAPINV[mv[0]] ] = 0
-    board [8 - int(mv[3])] [ LETTERMAPINV[mv[2]] ] = piece
+    return ret2
 
 
 
-def calc_move(white_turn=False):
+def make_move(mv):
+    piece = board[mv[0]][mv[1]]
+    hit_piece = board[mv[2]][mv[3]]
+    board[mv[0]][mv[1]] = 0
+    board[mv[2]][mv[3]] = piece
+    return hit_piece
+
+
+def unmake_move(mv, hit_piece):
+    piece = board[mv[2]][mv[3]]
+    board[mv[2]][mv[3]] = hit_piece
+    board[mv[0]][mv[1]] = piece
+
+
+def make_move_str(mv):
+    print('making move ' + mv)
+
+    a = 8 - int(mv[1])
+    b = LETTERMAPINV[mv[0]]
+    c = 8 - int(mv[3])
+    d = LETTERMAPINV[mv[2]]
+
+    make_move((a, b, c, d))
+
+    LASTMOVE = (a,b,c,d)
+
+
+def calc_move():
     mv = random.choice(gen_legals())
 
     piece = board[mv[0]][mv[1]]
     board[mv[0]][mv[1]] = 0
     board[mv[2]][mv[3]] = piece
+
+    LASTMOVE = mv
 
     # [print(row) for row in board]
 
@@ -101,38 +287,84 @@ def calc_move(white_turn=False):
 
 
 
-board = new_board()
+
+
+
+
+
+if len(sys.argv) >= 2:
+    if sys.argv[1].startswith('-t'):
+        king_check_test = [
+            [BLACK + KING, 0, 0, 0, WHITE + QUEEN, 0, 0, 0],
+            [0] * 8,
+            [0] * 8,
+            [0] * 8,
+            [0] * 8,
+            [0] * 8,
+            [0] * 8,
+            [0] * 8,
+        ]
+
+        board = king_check_test
+        assert not king_not_in_check()
+
+        exit()
+
+
+
 
 while True:
     i = input()
     ins = i.split(' ')[1:]
 
+    # work both interactively AND with xboard
     if i == 'xboard':
         print('feature myname="janchess"')
-        print('feature done=1')
         print('feature sigint=0 sigterm=0')  # else xboard sends KeyboardInterrupts
+        print('feature done=1')
+
+    elif i == 'white':
+        IM_WHITE = True
+        board = new_board()
 
     elif i.startswith('time'):
         total_time = int(ins[0])
         time_per_move = total_time / 100
 
+    elif i == 'force':
+        started = False
+
     elif i == 'new':
+        IM_WHITE = False
         board = new_board()
 
+    # answer pings
     elif i.startswith('ping'):
         print(i.replace('i', 'o'))
 
-    # elif i.startswith('MOVE') or i == '?':
-    elif input_is_move(i):
-        make_move(i)
-        time.sleep(.1)
+    elif input_is_move(i) and not started:
+        BUF = i
+
+    elif input_is_move(i) and started:
+        print('RECV MOVE', i)
+        make_move_str(i)
         print('move ' + calc_move())
 
+    # go/force stuff needed, else xboard skips second move
+    elif i == 'go':
+        started = True
+        if BUF:
+            make_move_str(BUF)
+            print('move ' + calc_move())
+            BUF = ''
+        else:
+            print('move ' + calc_move())
+
     elif i == 'quit' or i == 'q':
+        pprint()
         logging.shutdown()
         exit()
 
-    time.sleep(.01)
 
 
 
--- v001.py
+++ v001.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 
 
-from time import sleep
+# USAGE:
+# INTERACTIVE USAGE
+#   python3 v001.py
+# XBOARD -- SETUP
+#   chmod +x ./v001.py    # needed so xboard can execute us
+# XBOARD
+#   xboard -fcp ./v001.py -debug
+#                       # this will log to 'xboard.debug'
+#                       # in addition to 'v001.py.log' (this program)
+
+# this example file only makes some example moves (might be illegal!)
+#   as black then exits (unexpectedly)
+
+
+
+import time
 import logging
 import sys
 
-logging.basicConfig(filename='janchess.log', level=logging.DEBUG)
+logging.basicConfig(filename=sys.argv[0]+'.log', level=logging.DEBUG)
 
+# redefine builtins: now they also log to file
 def print(*args, **kwargs):
     __builtins__.print(*args, **kwargs)
     logging.info('>>> ' + ' '.join(str(m) for m in args))
@@ -17,38 +33,46 @@ def input(*args, **kwargs):
     return ret
 
 
-for i in range(100):
+
+move_number = 0
+
+moves = [
+    'move e7e5', # pawn
+    'move f8e7', # bishop
+    'move g8f6', # knight
+    'move b8c6', # knight
+    'move e8g8'  # castling
+]
+
+
+while True:
     i = input()
+
+    # work both interactively AND with xboard
     if 'xboard' in i:
-        # print('feature myname="janchess"')
+        print('feature myname="sys.argv[0]"')
+        print('feature sigint=0 sigterm=0')  # else xboard sends KeyboardInterrupts
         print('feature done=1')
+
+    # answer pings
     if i.startswith('ping'):
         print(i.replace('i', 'o'))
-    else:
-        print('move e7e5')
-    if 'q' == i:
+
+    # is a move like 'e2e4'
+    if len(i) == 4:
+        if i == 'post' or i == 'hard':
+            pass
+        elif move_number < len(moves):
+            print(moves[move_number])
+            move_number += 1
+        else:
+            i = 'q'
+
+    if i == 'q' or i == 'quit':
         logging.shutdown()
         exit()
-    sleep(.5)
 
-
-
-
-
-
-
-    # if i.startswith('feature'):
-    #     for item in i[len('feature'):]:
-    #         key, value = item.split('=')
-    #         print('accepted '+key)
-
-
-
-
-
-
-
-
+    time.sleep(.01)
 
 
 
